ea57cda0cd9e6fe88585716480b61ccc61b6f2af
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-04-27 13:17:15 -0300

    Linking static namespaces on FourChanThread.init

--- src/common.py
+++ src/common.py
@@ -2,4 +2,6 @@ import os
 
 MAIN_DIRECTORY = '/home/ignacio/misc/4chan/downchan'
 THREADS_DIRECTORY = os.path.join(MAIN_DIRECTORY, "threads")
+STATIC_DIRECTORY = os.path.join(MAIN_DIRECTORY, "static")
 
+STATIC_NAMESPACES = ["css", "js"]
